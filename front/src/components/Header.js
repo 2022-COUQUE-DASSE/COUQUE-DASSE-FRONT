2858a7f2ce240b91d9448c62d39090ad8cb39a67
민석 <80371412+MinSeok0123@users.noreply.github.com>
2022-12-06 17:22:12 +0900

    Fix :: Home페이지

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,7 +1,29 @@
+import "../css/Header.css";
+import { Link } from "react-router-dom";
 
-
-export function Header () {
-    return(
-        <div></div>
-    );
+export function Header() {
+  return (
+    <div className="headercenter">
+      <div className="header">
+        <Link to="/Picture" style={{ textDecoration: "none" }}>
+          <span className="headertext">
+            나의
+            <br /> 식물보기
+          </span>
+        </Link>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <img
+            className="cloud"
+            src="https://o.remove.bg/downloads/b3510341-5c20-4ac8-bf42-f26c4f4e65d4/image-removebg-preview.png"
+          ></img>
+        </Link>
+        <Link to="/Led" style={{ textDecoration: "none" }}>
+          <span className="headertext">
+            인공 햇빛 <br />
+            조절하기
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
+import { Header } from "../components/Header";
+import "../css/Home.css";
 
 export function Home() {
   return (
     <div>
-      홈페이지
-      <Link to="/Picture" style={{ textDecoration: "none" }}>
-        <div>사진페이지</div>
-      </Link>
-      <Link to="/Led" style={{ textDecoration: "none" }}>
-        <div>빛 조절 페이지</div>
-      </Link>
+      <Header />
+      <div className="Hometop">
+        {/* <Link to="/Picture" style={{ textDecoration: "none" }}>
+          <div>사진페이지</div>
+        </Link>
+        <Link to="/Led" style={{ textDecoration: "none" }}>
+          <div>빛 조절 페이지</div>
+        </Link> */}
+        <img
+          className="grass"
+          src="https://static.vecteezy.com/system/resources/previews/000/517/088/original/vector-landscape-illustration.png"
+        ></img>
+      </div>
     </div>
   );
 }
