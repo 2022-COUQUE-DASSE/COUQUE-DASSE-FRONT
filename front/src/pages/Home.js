43eb50564a95a90bf43c1c48bd4fea7c2ab2972f
민석 <80371412+MinSeok0123@users.noreply.github.com>
2022-12-12 16:02:16 +0900

    Feat :: tittle

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,29 +1,37 @@
 import { Link } from "react-router-dom";
 import { Header } from "../components/Header";
 import "../css/Home.css";
+import { Helmet } from "react-helmet";
 
 export function Home() {
   return (
     <div>
       <Header />
+      <Helmet>
+        <title>쿠쿠다스</title>
+      </Helmet>
       <img
         className="grass"
         src="https://static.vecteezy.com/system/resources/previews/000/517/088/original/vector-landscape-illustration.png"
       ></img>
       <div className="Homecenter">
-      <div className="Hometop">
-        <Link to="/Picture" style={{ textDecoration: "none" }} className="link">
-          <img className="boximg" src="/img/plant.png"></img>
-          <div className="picture"></div>
-          <span className="boxtext">나의 식물보기</span>
-        </Link>
-        <div className="block"></div>
-        <Link to="/Led" style={{ textDecoration: "none" }} className="link">
-          <img className="boximg" src="/img/bulb.png"></img>
-          <div className="picture"></div>
-          <span className="boxtext">인공 햇빛 조절하기</span>
-        </Link>
-      </div>
+        <div className="Hometop">
+          <Link
+            to="/Picture"
+            style={{ textDecoration: "none" }}
+            className="link"
+          >
+            <img className="boximg" src="/img/plant.png"></img>
+            <div className="picture"></div>
+            <span className="boxtext">나의 식물보기</span>
+          </Link>
+          <div className="block"></div>
+          <Link to="/Led" style={{ textDecoration: "none" }} className="link">
+            <img className="boximg" src="/img/bulb.png"></img>
+            <div className="picture"></div>
+            <span className="boxtext">인공 햇빛 조절하기</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
